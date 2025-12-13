885b244fd6c8a3cd4afad6104fab46bf485bb793
Muhammed Nihal MP <muhammednihalmp79@gmail.com>
2025-12-13 19:44:43 +0530

    Add login, register, and profile pages with routing
    Introduced Login, Register, and Profile pages, and updated App.jsx to include routes for these pages. The Profile page displays user info and embeds the new MyOrderPage component, which shows a mock order table. This enhances the authentication flow and user account management in the frontend.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import UserLayout from './Components/Layout/UserLayout';
 import Home from './Pages/Home';
 import { Toaster } from 'sonner';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
@@ -11,7 +14,10 @@ function App() {
       <Toaster position='top-right'/>
       <Routes>
         <Route path='/' element={<UserLayout />} >
-        <Route index element={<Home/>}/>
+          <Route index element={<Home/>}/>
+          <Route path='login' element={<Login/>}/>
+          <Route path='register' element={<Register/>}/>
+          <Route path='profile' element={<Profile/>}/>
         </Route>
       </Routes>
       </BrowserRouter>
